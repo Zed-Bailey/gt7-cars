86858e4a75a9597312158e23a78e1d9d33537b03
Zoran <emailzed03@gmail.com>
2024-04-04 12:12:49 +1100

    saved vehicles are now being displayed

--- app/(routes)/home/page.tsx
+++ app/(routes)/home/page.tsx
@@ -1,26 +1,64 @@
 'use client';
 
 import GetSupabaseClient from "@/app/_helpers/client";
-import { select } from "@nextui-org/react";
+import Car from "@/app/_models/Car";
+import { Button, select } from "@nextui-org/react";
 import { SupabaseClient } from "@supabase/supabase-js";
 import { useEffect, useState } from "react";
+import {TrashIcon} from "../../_icons/TrashIcon";
+import "/node_modules/flag-icons/css/flag-icons.min.css";
+
+
+interface SavedCar {
+    id: any;
+    name: any;
+    Country: {
+        name: any;
+        code: any;
+    };
+    Manufacturer: {
+        name: any;
+    };
+}
+
 
 export default function Home() {
 
 
     const [supabase, setSupabase] = useState<SupabaseClient>();
 
+    const [vehicles, setVehicles] = useState<SavedCar[] | null>([]);
+
     useEffect(() => {
         const client = GetSupabaseClient();
         setSupabase(client);
         
+
+        async function getCars(ids: string[]) {
+            const {data, error} = await client.from('Car')
+            .select(`
+                id, name, 
+                Country(name, code),
+                Manufacturer(name)
+            `)
+            .in('id', ids);
+        
+            if(error) {
+                console.log(error);
+            }
+
+            console.log("car data", data);
+            
+            return data;
+        }
+
         async function get() {
             const uid = (await client.auth.getSession()).data.session?.user.id;
             if(!uid) {
                 console.log("ERROR: no user id");
             }
 
-            const {data, error} = await client
+            var {data, error} = await client
                 .from('UserSubscriptions')
                 .select()
                 .eq('user_id', uid);
@@ -31,6 +69,10 @@ export default function Home() {
 
             console.log("data", data);
             
+            
+            let watchedCarIds: string[] = data![0].watched_cars;
+            let cars = await getCars(watchedCarIds);
+            setVehicles(cars);
         }
 
         get();
@@ -40,6 +82,40 @@ export default function Home() {
 
 
     return (
-        <div>Hello world</div>
+        <div>
+            Hello world
+            <br/>
+            {JSON.stringify(vehicles)}
+
+
+            <div className="flex flex-col gap-4">
+                {
+                    vehicles ? vehicles.map((x) => {
+                        return (
+                            <div key={x.id} className="group flex w-full max-w-screen-sm items-center justify-between border border-gray-400 bg-gray-900 rounded-lg p-3">
+
+                                <div className="flex items-center gap-4">
+                                    <div className="text-center min-w-20">
+                                        <span className={`fi fi-${x.Country.code}`}></span>
+                                        <p className="group-hover:opacity-100 opacity-0 text-xs transition-opacity duration-150">{x.Country.name}</p>
+                                    </div>
+                                    <div>
+                                        <h2 className="text-lg font-semibold">{x.name}</h2>
+                                        <p className="text-sm">{x.Manufacturer.name}</p>
+                                    </div>
+                                </div>
+                                
+                                
+                                <div className="">
+                                    <Button color="danger" variant="light" isIconOnly>
+                                        <TrashIcon className="fill-red-600"/>
+                                    </Button>
+                                </div>
+                            </div>
+                        );
+                    }) : null
+                }
+            </div>
+        </div>
     );
 }
