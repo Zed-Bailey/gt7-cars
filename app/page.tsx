cc60b4dd1c0de6499eb93dc1b0ee924bdbab08fc
Zoran <emailzed03@gmail.com>
2024-04-09 09:24:40 +1000

    styling the home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,11 @@
 
 import { Button, Input } from "@nextui-org/react";
 import Image from "next/image";
-import {MailIcon} from "./_icons/Mailicon";
 import { FormEvent } from "react";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const {push} = useRouter();
+  const { push } = useRouter();
 
   const submitSubscription = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -17,30 +16,62 @@ export default function Home() {
 
 
   return (
-      <main className="flex flex-col py-10 items-center">
+    <main className="flex flex-col  items-center bg-cover bg-fixed bg-center bg-no-repeat"
+      style={{ backgroundImage: `url("787b.jpg")` }} >
 
-        <section className="self-center">
-          
-          <div className="flex gap-3">
-            <div className="w-72 h-56 bg-gray-200"></div>
-            
-            <div>
-              <h1 className="text-3xl font-bold">Gran Turismo 7 legend cars notifier</h1>
-              <p>Get notified when a legend car comes into the dealership</p>
+      {/* <img className="w-full h-80 object-cover bg-fixed" src="787b.jpg"/> */}
+
+      <div className="mt-40 bg-black w-full">
+        <h1 className="text-center font-bold text-3xl mt-10">Gran Turismo 7 Dealership Notifier</h1>
+
+        <div className="flex flex-col w-full items-center">
+
+          {/* Legendary */}
+          <section className="flex gap-10 mt-20">
+
+            <div className="w-96 h-64 bg-gray-400"></div>
+
+            <div className="w-96">
+              <h2 className="text-2xl mb-3">Legend Cars</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur. Vitae auctor feugiat facilisis odio nec condimentum. Varius felis potenti in nibh tincidunt scelerisque sed urna eget.
+              </p>
             </div>
-            
-          </div>
-          
 
-        </section>
-        
+          </section>
 
-        <section className="w-full">
-          <h2>Login or Sign up</h2>
-          <Button color="primary" onClick={() => push('/auth/login')}>Login</Button>
-          <Button  variant="ghost" onClick={() => push('/auth/signup')}>Sign up</Button>
-        </section>
-        
-      </main>
+          {/* Used */}
+          <section className="flex gap-10  mt-20">
+
+
+            <div className="w-96">
+              <h2 className="text-2xl mb-3">Used Cars</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur. Vitae auctor feugiat facilisis odio nec condimentum. Varius felis potenti in nibh tincidunt scelerisque sed urna eget.
+              </p>
+            </div>
+
+            <div className="w-96 h-64 bg-gray-400"></div>
+          </section>
+
+
+
+
+          <section className="flex flex-col w-96 text-center mt-20 gap-4">
+            <h2 className="text-2xl">Get Started Now</h2>
+            <Button color="primary" onClick={() => push('/auth/login')}>Login</Button>
+            <Button variant="ghost" onClick={() => push('/auth/signup')}>Sign up</Button>
+          </section>
+
+        </div>
+
+        <div className="px-10 py-4 text-gray-600 ">
+          <span>Built by <a className="underline" href="https://github.com/Zed-Bailey">Zed</a></span>
+        </div>
+
+
+      </div>
+
+    </main>
   );
 }
